88ba26cdc304fc4b45db363b98c87184d4f8ffed
Gaganjeet <gaganjeetwork99@gmail.com>
2025-04-28 17:50:19 +0530

    Basic movie pagination working: Next/Previous + static 1-6 pages

--- Movzy/src/Component/pages/Movies.jsx
+++ Movzy/src/Component/pages/Movies.jsx
@@ -1,41 +1,88 @@
 import React, { useState, useEffect } from "react";
 import Card from "../card/Card";
+import "./Pagination.css";
 const Movies = () => {
   const [movie, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pageNumbers, setPageNumbers] = useState([1, 2, 3, 4, 5, 6]);
+
   const Movie_API = import.meta.env.VITE_MOVIE_API;
+
+  function handlePage(e) {
+    if (e.target.innerHTML === "Next") {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+    if (e.target.innerHTML === "Previous" && currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  }
 
   useEffect(() => {
     const fetchAllMovies = async () => {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${Movie_API}`
+          `https://api.themoviedb.org/3/movie/popular?api_key=${Movie_API}&page=${currentPage}`
         );
         if (!res.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await res.json();
-        const movie_arr = [...data.results];
-        setMovies(movie_arr);
+        setMovies(data.results.slice(0, 15));
+
+        setTotalPages(data.total_pages);
+        console.log(data.total_pages);
       } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
       }
     };
 
     fetchAllMovies();
-  }, []);
+  }, [currentPage]);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: "20px",
-        justifyContent: "center",
-      }}>
-      {movie.map((movie) => (
-        <Card key={movie.id} movie_mp={movie} />
-      ))}
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          justifyContent: "center",
+        }}>
+        {movie.map((movie) => (
+          <Card key={movie.id} movie_mp={movie} />
+        ))}
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span onClick={handlePage} className="sr-only">
+                Previous
+              </span>
+            </a>
+          </li>
+          {pageNumbers.map((pageNum) => (
+            <li key={pageNum} className="page-item">
+              <button
+                onClick={() => setCurrentPage(pageNum)}
+                className="page-link">
+                {pageNum}
+              </button>
+            </li>
+          ))}
+          <li className="page-item">
+            <a className="page-link" aria-label="Next">
+              <span onClick={handlePage} className="sr-only">
+                Next
+              </span>
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
--- Movzy/src/Component/pages/TV_Show.jsx
+++ Movzy/src/Component/pages/TV_Show.jsx
@@ -2,38 +2,89 @@ import React, { useState, useEffect } from "react";
 import Card from "../card/Card";
 const TV_Show = () => {
   const [show, setShow] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageNumbers, setPageNumbers] = useState([1, 2, 3, 4, 5, 6]);
+
   const Show_API = import.meta.env.VITE_MOVIE_API;
+
+  function handlePage(e) {
+    if (e.target.innerHTML === "Next") {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+    if (e.target.innerHTML === "Previous" && currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  }
 
   useEffect(() => {
     const fetchShow = async () => {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/tv/popular?api_key=${Show_API}`
+          `https://api.themoviedb.org/3/tv/popular?api_key=${Show_API}&page=${currentPage}`
         );
         if (!res.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await res.json();
-        const show_arr = [...data.results];
-        setShow(show_arr);
+        setShow(data.results.slice(0, 15));
       } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
       }
     };
     fetchShow();
-  }, []);
+  }, [currentPage]);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: "20px",
-        justifyContent: "center",
-      }}>
-      {show.map((show) => (
-        <Card key={show.id} show_mp={show} />
-      ))}
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          justifyContent: "center",
+        }}>
+        {show.map((show) => (
+          <Card key={show.id} show_mp={show} />
+        ))}
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span onClick={handlePage} className="sr-only">
+                Previous
+              </span>
+            </a>
+          </li>
+          {pageNumbers.map((page_no) => (
+            <li key={page_no} className="page-item">
+              <button
+                className="page-link"
+                onClick={() => setCurrentPage(page_no)}>
+                {page_no}
+              </button>
+            </li>
+          ))}
+          {/* <li className="page-item">
+            <a className="page-link">1</a>
+          </li>
+          <li className="page-item">
+            <a className="page-link">2</a>
+          </li>
+          <li className="page-item">
+            <a className="page-link">3</a>
+          </li> */}
+          <li className="page-item">
+            <a className="page-link" aria-label="Next">
+              <span onClick={handlePage} className="sr-only">
+                Next
+              </span>
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>{" "}
+    </>
   );
 };
 
