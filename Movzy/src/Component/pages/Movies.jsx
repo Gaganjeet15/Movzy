5592b451193648111daa86daf5c9d89bc8e4d8fa
Gaganjeet <gaganjeetwork99@gmail.com>
2025-04-29 19:31:10 +0530

    Add Movies & TV_Show page with filtering and pagination

--- Movzy/src/Component/pages/Movies.jsx
+++ Movzy/src/Component/pages/Movies.jsx
@@ -1,87 +1,62 @@
 import React, { useState, useEffect } from "react";
 import Card from "../card/Card";
-import "./Pagination.css";
+import "./Loader.css";
+import "../Pagination/Pagination.css";
+import Filter from "../Filter/Filter";
+import { MoveiefilterMap } from "../Filter/filterConfig";
+import Pagination from "../Pagination/Pagination";
+
 const Movies = () => {
   const [movie, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
   const [pageNumbers, setPageNumbers] = useState([1, 2, 3, 4, 5, 6]);
+  const [filterchange, setSelectedFilter] = useState("popular");
+  const [loading, setLoading] = useState(false);
 
-  const Movie_API = import.meta.env.VITE_MOVIE_API;
-
-  function handlePage(e) {
-    if (e.target.innerHTML === "Next") {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-    if (e.target.innerHTML === "Previous" && currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
+  function handleFilterChange(key) {
+    setSelectedFilter(key);
   }
 
   useEffect(() => {
     const fetchAllMovies = async () => {
       try {
+        const Movie_API = import.meta.env.VITE_MOVIE_API;
+        setLoading(true);
         const res = await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${Movie_API}&page=${currentPage}`
+          `https://api.themoviedb.org/3${MoveiefilterMap[filterchange]}?api_key=${Movie_API}&page=${currentPage}`
         );
         if (!res.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await res.json();
         setMovies(data.results.slice(0, 15));
-
-        setTotalPages(data.total_pages);
-        console.log(data.total_pages);
+        setLoading(false);
       } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
       }
     };
-
     fetchAllMovies();
-  }, [currentPage]);
+  }, [currentPage, filterchange]);
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "20px",
-          justifyContent: "center",
-        }}>
-        {movie.map((movie) => (
-          <Card key={movie.id} movie_mp={movie} />
-        ))}
-      </div>
-      <nav aria-label="Page navigation example">
-        <ul className="pagination">
-          <li className="page-item">
-            <a className="page-link" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-              <span onClick={handlePage} className="sr-only">
-                Previous
-              </span>
-            </a>
-          </li>
-          {pageNumbers.map((pageNum) => (
-            <li key={pageNum} className="page-item">
-              <button
-                onClick={() => setCurrentPage(pageNum)}
-                className="page-link">
-                {pageNum}
-              </button>
-            </li>
+      <Filter onFilterChange={handleFilterChange} type="movie" />
+      {loading ? (
+        <div className="loader-wrapper">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <div className="card-container">
+          {movie.map((movie) => (
+            <Card key={movie.id} movie_mp={movie} />
           ))}
-          <li className="page-item">
-            <a className="page-link" aria-label="Next">
-              <span onClick={handlePage} className="sr-only">
-                Next
-              </span>
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+        </div>
+      )}
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pageNumbers={pageNumbers}
+      />
     </>
   );
 };
--- Movzy/src/Component/pages/TV_Show.jsx
+++ Movzy/src/Component/pages/TV_Show.jsx
@@ -1,89 +1,58 @@
 import React, { useState, useEffect } from "react";
 import Card from "../card/Card";
+import Filter from "../Filter/Filter";
+import Pagination from "../Pagination/Pagination";
+import { tvFilterMap } from "../Filter/filterConfig";
+import "./Loader.css";
 const TV_Show = () => {
   const [show, setShow] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageNumbers, setPageNumbers] = useState([1, 2, 3, 4, 5, 6]);
+  const [filterchange, setSelectedFilter] = useState("popular");
+  const [loading, setLoading] = useState(false);
 
-  const Show_API = import.meta.env.VITE_MOVIE_API;
-
-  function handlePage(e) {
-    if (e.target.innerHTML === "Next") {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-    if (e.target.innerHTML === "Previous" && currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
+  function handleFilterChange(key) {
+    setSelectedFilter(key);
   }
-
   useEffect(() => {
     const fetchShow = async () => {
       try {
+        const Show_API = import.meta.env.VITE_MOVIE_API;
+        setLoading(true);
         const res = await fetch(
-          `https://api.themoviedb.org/3/tv/popular?api_key=${Show_API}&page=${currentPage}`
+          `https://api.themoviedb.org/3${tvFilterMap[filterchange]}?api_key=${Show_API}&page=${currentPage}`
         );
         if (!res.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await res.json();
         setShow(data.results.slice(0, 15));
+        setLoading(false);
       } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
       }
     };
     fetchShow();
-  }, [currentPage]);
+  }, [currentPage, filterchange]);
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "20px",
-          justifyContent: "center",
-        }}>
-        {show.map((show) => (
-          <Card key={show.id} show_mp={show} />
-        ))}
-      </div>
-      <nav aria-label="Page navigation example">
-        <ul className="pagination">
-          <li className="page-item">
-            <a className="page-link" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-              <span onClick={handlePage} className="sr-only">
-                Previous
-              </span>
-            </a>
-          </li>
-          {pageNumbers.map((page_no) => (
-            <li key={page_no} className="page-item">
-              <button
-                className="page-link"
-                onClick={() => setCurrentPage(page_no)}>
-                {page_no}
-              </button>
-            </li>
+      <Filter onFilterChange={handleFilterChange} type="tv" />
+      {loading ? (
+        <div className="loader-wrapper">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <div className="card-container">
+          {show.map((show) => (
+            <Card key={show.id} show_mp={show} />
           ))}
-          {/* <li className="page-item">
-            <a className="page-link">1</a>
-          </li>
-          <li className="page-item">
-            <a className="page-link">2</a>
-          </li>
-          <li className="page-item">
-            <a className="page-link">3</a>
-          </li> */}
-          <li className="page-item">
-            <a className="page-link" aria-label="Next">
-              <span onClick={handlePage} className="sr-only">
-                Next
-              </span>
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>{" "}
+        </div>
+      )}
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pageNumbers={pageNumbers}
+      />
     </>
   );
 };
