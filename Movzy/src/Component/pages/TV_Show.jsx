06d55f73104242265a116836f9a4bc965f8affc9
Gaganjeet <gaganjeetwork99@gmail.com>
2025-05-02 15:41:15 +0530

    added infinite scroll on Tv component

--- Movzy/src/Component/pages/TV_Show.jsx
+++ Movzy/src/Component/pages/TV_Show.jsx
@@ -7,52 +7,61 @@ import "./Loader.css";
 const TV_Show = () => {
   const [show, setShow] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageNumbers, setPageNumbers] = useState([1, 2, 3, 4, 5, 6]);
   const [filterchange, setSelectedFilter] = useState("popular");
-  const [loading, setLoading] = useState(false);
 
   function handleFilterChange(key) {
     setSelectedFilter(key);
   }
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   useEffect(() => {
     const fetchShow = async () => {
       try {
         const Show_API = import.meta.env.VITE_MOVIE_API;
-        setLoading(true);
         const res = await fetch(
           `https://api.themoviedb.org/3${tvFilterMap[filterchange]}?api_key=${Show_API}&page=${currentPage}`
         );
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
+        if (!res.ok) throw new Error("Network response was not ok");
+
         const data = await res.json();
-        setShow(data.results.slice(0, 15));
-        setLoading(false);
+        setShow((prev) => [...prev, ...data.results]); // No slice unless needed
       } catch (error) {
-        console.error("There was a problem with the fetch operation:", error);
+        console.error("Fetch error:", error);
       }
     };
     fetchShow();
   }, [currentPage, filterchange]);
+
+  useEffect(() => {
+    setShow([]);
+    setCurrentPage(1);
+  }, [filterchange]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       <Filter onFilterChange={handleFilterChange} type="tv" />
-      {loading ? (
-        <div className="loader-wrapper">
-          <div className="loader"></div>
-        </div>
-      ) : (
-        <div className="card-container">
-          {show.map((show) => (
-            <Card key={show.id} show_mp={show} />
-          ))}
-        </div>
-      )}
-      <Pagination
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        pageNumbers={pageNumbers}
-      />
+      <div className="card-container">
+        {show.map((show) => (
+          <Card key={show.id} show_mp={show} />
+        ))}
+      </div>
     </>
   );
 };
